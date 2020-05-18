1355d58d0248c84788874cca8d87df161c84e638
rockdam <choi37338@naver.com>
2020-05-19 00:16:07 +0900

    github 잔디 심어지지 않는 문제 해결

---
+++ main_mango/app/src/main/java/com/softSquared/mangoplate/src/home/discount/interfaces/DiscountFragmentView.java
@@ -0,0 +1,8 @@
+package com.softSquared.mangoplate.src.home.discount.interfaces;
+
+import com.softSquared.mangoplate.src.home.discount.models.EatDealsResponse;
+
+public interface DiscountFragmentView {
+
+    void successUpdateRecyclerView(EatDealsResponse eatDealsResponse);
+}
