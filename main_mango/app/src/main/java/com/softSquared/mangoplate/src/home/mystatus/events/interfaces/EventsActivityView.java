92a91aacc17828a0610ccb23ad2c255762873d89
choirockdam <choi37338@naver.com>
2020-05-18 23:54:03 +0900

    events 부분 뷰 서비스 분리 .

---
+++ main_mango/app/src/main/java/com/softSquared/mangoplate/src/home/mystatus/events/interfaces/EventsActivityView.java
@@ -0,0 +1,9 @@
+package com.softSquared.mangoplate.src.home.mystatus.events.interfaces;
+
+import com.softSquared.mangoplate.src.home.mystatus.events.models.EventsResponse;
+
+public interface EventsActivityView {
+
+    void successUpdateRecyclerView(EventsResponse eventsResponse);
+
+}
