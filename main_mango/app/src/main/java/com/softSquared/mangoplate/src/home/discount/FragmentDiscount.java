75fe9b74703588688bb3182f4a4c02e6d435167b
rockdam <choi37338@naver.com>
2020-05-19 00:18:55 +0900

    잔디를 심기 위해서는 아이디가 같아야한다..

--- main_mango/app/src/main/java/com/softSquared/mangoplate/src/home/discount/FragmentDiscount.java
+++ main_mango/app/src/main/java/com/softSquared/mangoplate/src/home/discount/FragmentDiscount.java
@@ -2,6 +2,7 @@ package com.softSquared.mangoplate.src.home.discount;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -49,6 +50,7 @@ public class FragmentDiscount extends Fragment {
         mRootView = (ViewGroup) inflater.inflate(R.layout.fragment_discount, container, false);
 
 
+        Log.e("dd","잔디가 안 심어지는 이유가 있었다..")
         return mRootView;
     }
 
